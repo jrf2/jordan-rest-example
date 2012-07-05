789089f3bec3881abf781b6bd6a08a7de8e57c81
jrf2 <jrf2@sfu.ca>
2012-07-04 18:58:56 -0700

    adding model and database scripts written by yukai

--- routes/index.js
+++ routes/index.js
@@ -3,188 +3,36 @@ var config = JSON.parse(fs.readFileSync('config.json'));
 var mysql = require("mysql");
 var dbname = config.database.dbname;
 
+var queryES = require('./queryES.js');
+var question = require('./../models/question.js');
+
 exports.index = function(request, response) {
 	response.render('index', { title: "Homepage", username: checkAuth(request), _csrf: request.session_csrf });
 }
 
-exports.user = function(request, response) {
+exports.question = function(request, response) {
 	if (request.headers['content-type'] === 'application/json') {
 		if (request.method === "GET") {
 			var username = request.body.id;
 			
 			response.writeHead(501, { 'Content-Type': 'application/json' });
 			response.end(JSON.stringify({ message: 'not implemented' }));
+			
 		} else if (request.method === "POST") {
+		
 			response.writeHead(501, { 'Content-Type': 'application/json' });
 			response.end(JSON.stringify({ message: 'not implemented' }));
+			
 		} else if (request.method === "PUT") {
+		
 			response.writeHead(501, { 'Content-Type': 'application/json' });
 			response.end(JSON.stringify({ message: 'not implemented' }));
+			
 		} else if (request.method === "DELETE") {
+		
 			response.writeHead(501, { 'Content-Type': 'application/json' });
 			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.course = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.file = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.tag = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.studygroup = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.resource = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.like = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.question = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.answer = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		}
-	}
-}
-
-exports.comment = function(request, response) {
-	if (request.headers['content-type'] === 'application/json') {
-		if (request.method === "GET") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "POST") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "PUT") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
-		} else if (request.method === "DELETE") {
-			response.writeHead(501, { 'Content-Type': 'application/json' });
-			response.end(JSON.stringify({ message: 'not implemented' }));
+			
 		}
 	}
 }
